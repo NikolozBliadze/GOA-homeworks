2470da00797cd09c49c1cc6f4869cff2765ef553
NikolozBliadze <bliadzen19@gmail.com>
2024-12-08 13:15:05 +0400

    commit

--- Day 26/homework/hw1.py
+++ Day 26/homework/hw1.py
@@ -21,3 +21,31 @@ color("red")
 triangle("red", -100, -100)
 
 exitonclick()
+
+
+def print_hello_world():
+   message = "hello, world"
+   print(message)
+
+print_hello_world()
+
+
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        print(f"{number} არის ლუწი")
+    else:
+        print(f"{number} არის კენტი")
+
+even_or_odd(23)
+even_or_odd(42)
+
+
+def cube():
+    for i in range(120):
+        print("******")
+
+cube()
+
+
+    
